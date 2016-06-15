bc49b53ed01e208a38bfcb53731473155a4a2c74
Marwan Zogheib <mzogheib@gmail.com>
2016-06-15 22:12:53 +1000

    Add moment.js, minor restructure of template, perform basic calculation

--- js/app/calculator/calculator.component.js
+++ js/app/calculator/calculator.component.js
@@ -7,21 +7,40 @@ angular.
         templateUrl: 'js/app/calculator/calculator.template.html',
         controller: function TimeCalcController() {
             var self = this;
-            
-            self.fromDate = 0;
-            
-            self.modYears = 0;
-            self.modMonths = 0;
-            self.modWeeks = 0;
-            self.modDays = 0;
-            self.modHours = 0;
-            self.modMinutes = 0;
-            self.modSeconds = 0;
-            
-            self.toDate = 0;
-            
-            self.increment = function increment () {
-                self.toDate = self.fromDate + self.modYears;
+
+            self.calculate = calculate;
+            self.reset = reset;
+
+            init();
+
+            function init () {
+                self.fromTimestamp = moment();
+
+                self.modYears = 0;
+                self.modMonths = 0;
+                self.modWeeks = 0;
+                self.modDays = 0;
+                self.modHours = 0;
+                self.modMinutes = 0;
+                self.modSeconds = 0;
+
+                self.toTimestamp = null;
+            }
+
+            function calculate () {
+                self.toTimestamp = self.fromTimestamp
+                    .clone()
+                    .add(self.modYears, 'years')
+                    .add(self.modMonths, 'months')
+                    .add(self.modWeeks, 'weeks')
+                    .add(self.modDays, 'days')
+                    .add(self.modHours, 'hours')
+                    .add(self.modMinutes, 'minutes')
+                    .add(self.modSeconds, 'seconds');
+            }
+
+            function reset () {
+                init();
             }
         }
     });
