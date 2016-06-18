24a1f02f0c012a531c422e074e00b519ba131b2f
Marwan Zogheib <mzogheib@gmail.com>
2016-06-18 21:44:05 +1000

    Calculate mod values if to values have changed, add form validation to all fields (except year), remove weeks

--- js/app/calculator/calculator.component.js
+++ js/app/calculator/calculator.component.js
@@ -21,15 +21,14 @@ angular.
 
                 self.fromYear = self.fromTimestamp.year();
                 self.fromMonth = self.fromTimestamp.month();
-                self.fromWeek = self.fromTimestamp.week();
                 self.fromDate = self.fromTimestamp.date();
                 self.fromHour = self.fromTimestamp.hour();
                 self.fromMinute = self.fromTimestamp.minute();
                 self.fromSecond = self.fromTimestamp.second();
+                self.fromEOM = self.fromTimestamp.clone().endOf("month").date();
 
                 self.modYears = 0;
                 self.modMonths = 0;
-                self.modWeeks = 0;
                 self.modDays = 0;
                 self.modHours = 0;
                 self.modMinutes = 0;
@@ -47,13 +46,32 @@ angular.
                     .minute(self.fromMinute)
                     .second(self.fromSecond);
 
+                self.fromEOM = self.fromTimestamp.clone().endOf("month").date();
+
                 self.fromTimestampFormatted = self.fromTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a");
 
                 calculateTo();
             }
 
             function calculateMod () {
-                console.log('Yo');
+                self.toTimestamp
+                    .year(self.toYear)
+                    .month(self.toMonth)
+                    .date(self.toDate)
+                    .hour(self.toHour)
+                    .minute(self.toMinute)
+                    .second(self.toSecond);
+
+                self.toEOM = self.toTimestamp.clone().endOf("month").date();
+
+                self.toTimestampFormatted = self.toTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+                self.modYears = self.toYear - self.fromYear;
+                self.modMonths = self.toMonth - self.fromMonth;
+                self.modDays = self.toDate - self.fromDate;
+                self.modHours = self.toHour - self.fromHour;
+                self.modMinutes = self.toMinute - self.fromMinute;
+                self.modSeconds = self.toSecond - self.fromSecond;
             }
 
             function calculateTo () {
@@ -61,17 +79,17 @@ angular.
                     .clone()
                     .add(self.modYears, 'years')
                     .add(self.modMonths, 'months')
-                    .add(self.modWeeks, 'weeks')
                     .add(self.modDays, 'days')
                     .add(self.modHours, 'hours')
                     .add(self.modMinutes, 'minutes')
                     .add(self.modSeconds, 'seconds');
 
+                self.toEOM = self.toTimestamp.clone().endOf("month").date();
+
                 self.toTimestampFormatted = self.toTimestamp.format("dddd, MMMM Do YYYY, h:mm:ss a");
 
                 self.toYear = self.toTimestamp.year();
                 self.toMonth = self.toTimestamp.month();
-                self.toWeek = self.toTimestamp.week();
                 self.toDate = self.toTimestamp.date();
                 self.toHour = self.toTimestamp.hour();
                 self.toMinute = self.toTimestamp.minute();
