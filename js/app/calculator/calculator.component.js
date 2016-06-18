64738e95d310bec86807065401bfc68fd4301edf
Marwan Zogheib <mzogheib@gmail.com>
2016-06-18 18:14:22 +1000

    Change layout to columns, add weeks field (disabled), add ability to update from date & time, calculate automatically rather than with a button

--- js/app/calculator/calculator.component.js
+++ js/app/calculator/calculator.component.js
@@ -5,16 +5,26 @@ angular.
     module('calculator').
     component('calculator', {
         templateUrl: 'js/app/calculator/calculator.template.html',
-        controller: function TimeCalcController() {
+        controller: function TimeCalcController($scope) {
             var self = this;
 
-            self.calculate = calculate;
+            self.calculateFrom = calculateFrom;
+            self.calculateMod = calculateMod;
+            self.calculateTo = calculateTo;
             self.reset = reset;
 
             init();
 
             function init () {
                 self.fromTimestamp = moment();
+
+                self.fromYear = self.fromTimestamp.year();
+                self.fromMonth = self.fromTimestamp.month();
+                self.fromWeek = self.fromTimestamp.week();
+                self.fromDate = self.fromTimestamp.date();
+                self.fromHour = self.fromTimestamp.hour();
+                self.fromMinute = self.fromTimestamp.minute();
+                self.fromSecond = self.fromTimestamp.second();
 
                 self.modYears = 0;
                 self.modMonths = 0;
@@ -24,10 +34,26 @@ angular.
                 self.modMinutes = 0;
                 self.modSeconds = 0;
 
-                self.toTimestamp = null;
+                calculateTo();
             }
 
-            function calculate () {
+            function calculateFrom () {
+                self.fromTimestamp
+                    .year(self.fromYear)
+                    .month(self.fromMonth)
+                    .date(self.fromDate)
+                    .hour(self.fromHour)
+                    .minute(self.fromMinute)
+                    .second(self.fromSecond);
+
+                calculateTo();
+            }
+
+            function calculateMod () {
+                console.log('Yo');
+            }
+
+            function calculateTo () {
                 self.toTimestamp = self.fromTimestamp
                     .clone()
                     .add(self.modYears, 'years')
@@ -37,6 +63,14 @@ angular.
                     .add(self.modHours, 'hours')
                     .add(self.modMinutes, 'minutes')
                     .add(self.modSeconds, 'seconds');
+
+                self.toYear = self.toTimestamp.year();
+                self.toMonth = self.toTimestamp.month();
+                self.toWeek = self.toTimestamp.week();
+                self.toDate = self.toTimestamp.date();
+                self.toHour = self.toTimestamp.hour();
+                self.toMinute = self.toTimestamp.minute();
+                self.toSecond = self.toTimestamp.second();
             }
 
             function reset () {
